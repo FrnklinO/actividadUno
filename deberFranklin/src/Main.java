5ff8f69f164d491bb9a26b72164a387090fb49b4
Gladys Masaquiza <gladysm_12masa@hotmail.com>
2023-06-10 14:52:35 -0500

    se agrega deber indicado

---
+++ deberFranklin/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        instructions variable = new instructions();
+        variable.Especificar();
+
+        System.out.println("\nAdios!");
+    }
+}
